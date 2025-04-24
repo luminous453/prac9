d428df4567b1e8b5693a87406c96dc085d1e72d0
luminous <masha4russia@gmail.com>
2025-04-25 02:21:53 +0300

    2 commit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
-console.log('Happy developing ✨')
+const express = require('express');
+const app = express();
+const PORT = 5000;
+app.get('/api', (req, res) => {
+    res.send('Hello from Backend!');
+});
+app.listen(PORT, () => {
+    console.log(`Backend running on port ${PORT}`);
+});
