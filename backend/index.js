4a7731f30dc1857ab3276863aa5d73f7081fcd73
luminous <masha4russia@gmail.com>
2025-04-25 02:27:22 +0300

    3 commit

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 app.get('/api', (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
     res.send('Hello from Backend!');
 });
 app.listen(PORT, () => {
--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-fetch('http://backend:5000/api')
+fetch('http://localhost:5000/api')
     .then(response => response.text())
     .then(data => {
         document.getElementById('response').textContent = data;
